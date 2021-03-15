e1ce8866dc525c8cf9bd7ae3f5f8a559992ec938
Banzak-Uninter <leosantanabr@gmail.com>
2021-03-15 18:54:44 -0300

    Desafio Objects

---
+++ objects/index.js
@@ -0,0 +1,15 @@
+// Criar um programa que calcula media dos alunos.
+
+//Programa para impressão de objetos
+
+const empresa = {
+    nome: "Rocketseat",
+    cor: "Roxo",
+    foco: "Programação",
+    endereco: {
+        rua: "Rua Guilherme Gembala",
+        numero: 260
+    }
+}
+
+console.log(`A empresa ${empresa.nome} está localizada em ${empresa.endereco.rua}, ${empresa.endereco.numero}.`)

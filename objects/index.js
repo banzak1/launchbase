fa7a6a0c928af075bb115d4ee4120c02da39530d
banzak1 <leosantanabr@gmail.com>
2021-03-15 23:54:20 -0300

    Desafio 02 Objects

--- objects/index.js
+++ objects/index.js
@@ -2,7 +2,7 @@
 
 //Programa para impressão de objetos
 
-const empresa = {
+const empresa01 = {
     nome: "Rocketseat",
     cor: "Roxo",
     foco: "Programação",
@@ -12,4 +12,16 @@ const empresa = {
     }
 }
 
-console.log(`A empresa ${empresa.nome} está localizada em ${empresa.endereco.rua}, ${empresa.endereco.numero}.`)
+console.log(`A empresa ${empresa01.nome} está localizada em ${empresa01.endereco.rua}, ${empresa01.endereco.numero}.`)
+
+const empresa02 = {
+    nome: "Banzak Ltda",
+    cor: "Ciano",
+    foco: "Jogos",
+    endereco: {
+        rua: "Rua Volta do Tanque",
+        numero: 165
+    }
+}
+
+console.log(`A empresa ${empresa02.nome} com foco em ${empresa02.foco} está localizada na ${empresa02.endereco.rua}. ${empresa02.endereco.numero}`)

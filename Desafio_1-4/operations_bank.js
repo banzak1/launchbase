d0fd6107e3b1a34dbdd5a4c175fbdacc47d5e1c7
Banzak-Uninter <leosantanabr@gmail.com>
2021-03-23 16:23:09 -0300

    add showHigherTransaction function

--- Desafio_1-4/operations_bank.js
+++ Desafio_1-4/operations_bank.js
@@ -5,26 +5,41 @@ const user = {
     }
 
 
-    function createTransaction(transaction) {
-        user.transactions.push(transaction)
-    
-        if (transaction.type === 'credit') {
-            user.balance = user.balance + transaction.value
-        } else {
-            user.balance = user.balance - transaction.value
+function createTransaction(transaction) {
+    user.transactions.push(transaction)
+
+    if (transaction.type === 'credit') {
+        user.balance = user.balance + transaction.value
+    } else {
+        user.balance = user.balance - transaction.value
+    }
+}
+
+function getHigherTransactionByType(type) {
+    var higherTransaction
+    let higherValue = 0
+
+    for (let transaction of user.transactions) {
+        if (transaction.type === type && transaction.value > higherValue){
+            higherValue = transaction.value
+            higherTransaction = transaction
         }
     }
-    
-    function getHigherTransactionByType(type) {
-        let higherTransaction
-        let higherValue = 0
-    
-        for (let transaction of user.transactions) {
-            if (transaction.type === type && transaction.value > higherValue){
-                higherValue = transaction.value
-                higherTransaction = transaction
-            }
-        }
-        return higherTransaction
-    }
-    
+    return  console.log(higherTransaction)
+}
+
+function showHigherTransaction(type) {
+
+    getHigherTransactionByType(type)
+
+}
+
+createTransaction({ type: 'credit', value: 50 })
+createTransaction({ type: 'credit', value: 120 })
+createTransaction({ type: 'debit', value: 80 })
+createTransaction({ type: 'debit', value: 30 })
+
+console.log(user.balance)
+
+showHigherTransaction('credit')
+

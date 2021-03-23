bf570b85773a9d1fcc74f416b7bcf7d5b33709ed
Banzak-Uninter <leosantanabr@gmail.com>
2021-03-23 16:39:47 -0300

    add getAverageTransactionValue

--- Desafio_1-4/operations_bank.js
+++ Desafio_1-4/operations_bank.js
@@ -2,7 +2,7 @@ const user = {
         name: 'Leonardo',
         transactions: [],
         balance: 0
-    }
+}
 
 
 function createTransaction(transaction) {
@@ -29,9 +29,20 @@ function getHigherTransactionByType(type) {
 }
 
 function showHigherTransaction(type) {
-
     getHigherTransactionByType(type)
+}
 
+function getAverageTransactionValue() {
+    let transactionValue = 0
+    let averageTransactionValue
+    let transaction = user.transactions
+
+    for (let transtion of user.transactions) {
+        transactionValue = transactionValue + transtion.value
+    }
+    averageTransactionValue = transactionValue / transaction.length
+
+    return console.log(averageTransactionValue)
 }
 
 createTransaction({ type: 'credit', value: 50 })
@@ -42,4 +53,6 @@ createTransaction({ type: 'debit', value: 30 })
 console.log(user.balance)
 
 showHigherTransaction('credit')
+showHigherTransaction('debit')
 
+getAverageTransactionValue()
